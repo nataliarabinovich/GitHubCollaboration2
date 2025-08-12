d7b436c664d7723edf7fb256e889985709b6d135
Maya <mayabjj104@gmail.com>
2025-08-12 13:54:00 -0400

    border weight for nav bar

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -7,17 +7,10 @@
 
 import SwiftUI
 struct ContentView: View {
-    
     var body: some View {
-        
-        
-        
-        
         VStack {
-      
-        
             Text("AthlEats")
-                .padding(.top, -225.0)
+            //.padding(.top, -200.0)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
             // Image(systemName: "globe")
@@ -25,18 +18,49 @@ struct ContentView: View {
             // .foregroundStyle(.tint)
             Text("Welcome back, name")
                 .fontWeight(.medium)
-                .padding(.vertical, 100.0)
+                .font(.system(size: 25))
+                .padding(.vertical, 200.0)
             Button("Quiz") {
-                
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
+            .font(.system(size: 20))
+            .frame(width: 80.0, height: 30.0)
             .border(.blue)
-        Divider()
+            
+            
+            // .padding([.top, .leading, .bottom])
+            Spacer()
+            Divider()
+                .frame(height:3)
+                .background(Color.gray)
+            
+            //  fontWeight(_Weight:20)
+            HStack{
+                Button("🏠") {
+                    
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                Button("  ⚽️") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                Button("  🍴") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                Button("  ❤️") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+            }
         }
     }
-}
-    
-
-#Preview {
- ContentView()
-}
+         }
+         #Preview {
+             ContentView()
+         }
+     
