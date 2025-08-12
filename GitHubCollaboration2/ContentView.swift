53c0b67b924e2e2b732c4173f3fc27d93f700e2a
Rebecca Collins <rebeccacollins590@gmail.com>
2025-08-12 13:49:52 -0400

    updated code with navigation bar

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -9,56 +9,67 @@ import SwiftUI
 
 
 struct ContentView: View {
-  var body: some View {
-    VStack {
-      Text("AthlEats")
-        //.padding(.top, -200.0)
-        .font(.largeTitle)
-        .fontWeight(.semibold)
-    //  Image(systemName: "globe")
-        // .imageScale(.large)
-       //  .foregroundStyle(.tint)
-        Text("Welcome back, name")
-            .font(.title3)
-            .fontWeight(.medium)
-            .padding(.vertical, 200.0)
-      Button("Quiz") {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-          
-      }
-      .frame(width: 80.0, height: 30.0)
-        
-      .border(.blue)
-    
-     // .padding([.top, .leading, .bottom])
-    
-        Spacer()
-        Divider()
-            .padding(.bottom, 50.0)
-        Button("🏠         ⚽️        🍴         ❤️") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+    var body: some View {
+        VStack {
+            Text("AthlEats")
+            //.padding(.top, -200.0)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            //  Image(systemName: "globe")
+            // .imageScale(.large)
+            //  .foregroundStyle(.tint)
+            Text("Welcome back, name")
+                .font(.title2)
+                .fontWeight(.medium)
+                .padding(.vertical, 200.0)
+            Button("Quiz") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                
+            }
+            .frame(width: 80.0, height: 30.0)
+            
+            .border(.blue)
+            
+            // .padding([.top, .leading, .bottom])
+            
+            Spacer()
+            Divider()
+                .padding(.bottom, 10.0)
+           
+            HStack{
+                Button("🏠") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                
+                Button("    ⚽️") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                
+                Button("     🍴") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                
+                Button("    ❤️") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                .padding(.vertical)
+                
+                
+            }
         }
-    
     }
-  }
-}
-#Preview {
-  ContentView()
 }
 
-                            
-                        
-        
-
-
-
-
-
-
-
-
-
-
-
+    
+    #Preview {
+        ContentView()
+    }
 
 
