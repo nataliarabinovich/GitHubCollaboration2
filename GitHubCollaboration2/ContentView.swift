e2710decc4daaadd20edf0e8ef9a33a0ef8c0444
Maya <mayabjj104@gmail.com>
2025-08-12 11:57:24 -0400

    proper spacing

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -11,22 +11,21 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("AthlEats")
-                
-                .padding()
+                .padding(.top, -225.0)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Welcome back, name")
-          
+                .padding(.vertical, 100.0)
             Button("Quiz") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 
             }
             
         }
-        .padding()
+        
     }
 }
 
