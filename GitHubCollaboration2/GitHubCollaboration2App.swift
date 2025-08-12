8dbdd4961731202919e02a2108b463fdc2e2ffe2
Natalia <natalia.rabinovich@icloud.com>
2025-08-12 09:37:42 -0400

    Initial Commit

---
+++ GitHubCollaboration2/GitHubCollaboration2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaboration2App.swift
+//  GitHubCollaboration2
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaboration2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
