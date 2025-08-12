2ae83ad3c182678803dc21343bd1ed1b056e6631
Rebecca Collins <rebeccacollins590@gmail.com>
2025-08-12 13:28:21 -0400

    added buttons

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -7,38 +7,48 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
   var body: some View {
     VStack {
       Text("AthlEats")
-            .padding(108.0)
+        //.padding(.top, -200.0)
         .font(.largeTitle)
         .fontWeight(.semibold)
     //  Image(systemName: "globe")
-         //.imageScale(.large)
-      //  .foregroundStyle(.tint)
-      Text("Welcome back, name")
+        // .imageScale(.large)
+       //  .foregroundStyle(.tint)
+        Text("Welcome back, name")
+            .font(.title3)
+            .fontWeight(.medium)
+            .padding(.vertical, 200.0)
       Button("Quiz") {
-         
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
           
       }
-    }
-   
-  }
-
-    
- 
+      .frame(width: 80.0, height: 30.0)
         
-       
+      .border(.blue)
+    
+     // .padding([.top, .leading, .bottom])
+    
+        Spacer()
+        Divider()
+            .padding(.bottom, 50.0)
+        Button("🏠         ⚽️        🍴         ❤️") {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
     
-                            
-                        
-        
-    
+    }
+  }
+}
 #Preview {
   ContentView()
 }
+
+                            
+                        
+        
 
 
 
