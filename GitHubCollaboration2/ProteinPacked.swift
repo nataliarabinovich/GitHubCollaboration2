ef03f056ee37ed226f033d73c17653259ae365eb
Maya <mayabjj104@gmail.com>
2025-08-13 14:57:20 -0400

    working links for protein

--- GitHubCollaboration2/ProteinPacked.swift
+++ GitHubCollaboration2/ProteinPacked.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ProteinPacked: View {
     var body: some View {
-        
+        Text("Protein-Packed")
+            .font(.title)
+            .fontWeight(.semibold)
         List {
-            Link("Try this first recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
-            Link("Try this second recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
-            Link("Try this third recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
-        }//end of list
+            Link("Lemon Brown Butter Salmon", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
+            Link("Chicken Stir-Fry!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a45362568/best-chicken-stir-fry-recipe/")!)
+            Link("Garlic Shrimp and Veggies", destination: URL(string: "https://www.delish.com/cooking/recipes/a51341/sheet-pan-garlicky-shrimp-and-veggies-recipe/")!)
+        }
+        .padding(.bottom, 100.0)//end of list
         
         
         
