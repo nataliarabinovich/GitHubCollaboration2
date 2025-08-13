61979f2c19c00099afbf326ec18782dc90d02fc1
ava <ava.cg.lee@gmail.com>
2025-08-13 14:51:47 -0400

    added MyHealth

--- GitHubCollaboration2/MyHealth.swift
+++ GitHubCollaboration2/MyHealth.swift
@@ -10,21 +10,47 @@ import SwiftUI
 struct MyHealth: View {
     var body: some View {
         VStack{
+            Spacer().frame(height: 20)
             Text("My Health")
-                .font(.title)
-                .fontWeight(.bold)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
                 .foregroundColor(Color.black)
-            
-           Spacer()
-                .padding()
+                //.padding()
             Divider()
                 .frame(height: 4)
                 .overlay(Color.black)
-            Spacer ()
+                .frame(height:40)
+                .padding()
+            Spacer()
+            VStack{
+                Text("Meal Tracker:")
+                    .font(.system(size: 30))
+                    .fontWeight(.bold)
+                Text("Add meal +")
+                Spacer()
+                Spacer()
+                Text("Daily Steps:")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("12,473 steps")
+                Spacer()
+                Spacer()
+                Text("Calories:")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("3,028 calories")
+                Spacer()
+                    .frame(height:400)
+                
+            }
+            .background {
+                Color(hue: 0.600, saturation: 0.302, brightness: 1.1)
+                    .ignoresSafeArea()
+            }
         }
+        
     }
-  
-  
+
     }
 #Preview {
     MyHealth()
