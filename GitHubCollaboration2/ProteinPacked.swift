a5c85a89a2c565b6ee4f0c91bd5434213e433cd6
Maya <mayabjj104@gmail.com>
2025-08-13 14:49:51 -0400

    functioning first recipe

--- GitHubCollaboration2/ProteinPacked.swift
+++ GitHubCollaboration2/ProteinPacked.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct ProteinPacked: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        List {
+            Link("Try this first recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
+            Link("Try this second recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
+            Link("Try this third recipe!", destination: URL(string: "https://www.delish.com/cooking/recipe-ideas/a63205276/lemon-brown-butter-salmon-recipe/")!)
+        }//end of list
+        
+        
+        
+    }//end of body
+}//end of struct ProteinPacked
+
 
 #Preview {
     ProteinPacked()
